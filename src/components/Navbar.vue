52e5a44ac231ff9de838b6e0fdf157761d09b23b
Luke Stainer <lstainer2002@gmail.com>
2024-02-18 23:30:29 +1030

    Started doing navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<script>
+    export default {
+        data() {
+            return {
+            menuExpanded: false,
+            };
+        },
+        methods: {
+            toggleMenu() {
+            console.log("Toggled menu to " + this.menuExpanded);
+            this.menuExpanded = !this.menuExpanded;
+            },
+        }
+    }
+</script>
+
+<template>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <div class="navbar">
+        <a v-if="menuExpanded" href="" class="nav-item">Home</a>
+        <a v-if="menuExpanded" href="" class="nav-item">About Us</a>
+        <a v-if="menuExpanded" href="" class="nav-item">Contact Us</a>
+        <a v-if="menuExpanded" href="" class="nav-item">FAQ</a>
+        <a href="javascript:void(0);" class="toggle" @click="toggleMenu()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+</template>
+
+<style scoped>
+/* Mobile design */
+.navbar {
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: 1fr;
+    text-align: center;
+
+    background-color: rgb(51, 51, 51);
+}
+
+/* Desktop design - apply when the screen width is 1024px or larger */
+@media (min-width: 1024px) {
+    .navbar {
+        grid-template-columns: repeat(auto-fill, minmax(240px, 1fr)); /* Multiple columns on desktop */
+    }
+}
+</style>
