01e70ea12f977128c4fcdcaf25a13ef5338c56f2
WukoSiric <lstainer2002@gmail.com>
2024-02-19 19:07:47 +1030

    Remove some default css

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,11 @@ import Navbar from './components/Navbar.vue'
     <Navbar></Navbar>
   </header>
   <main>
+    <h1>This is my big heading</h1>
+    <p>As you can see I have a lot to say inside of here...</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae libero nesciunt maiores provident cumque, molestiae reprehenderit magni voluptatibus? Quod deserunt exercitationem ipsam veritatis quam numquam labore iure vitae dolor molestiae!</p>
   </main>
 </template>
 
 <style scoped>
-
-@media (min-width: 1024px) {
-}
 </style>
