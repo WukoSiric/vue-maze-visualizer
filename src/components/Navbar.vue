58c996a564fd28fba5a79c20d258f3b61814563d
WukoSiric <lstainer2002@gmail.com>
2024-02-21 01:44:23 +1030

    Burger mobile and desktop functionality done

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,14 +2,24 @@
     export default {
         data() {
             return {
-            menuExpanded: false,
+                menuExpanded: false,
+                isMobile: window.innerWidth < 1024
             };
         },
         methods: {
             toggleMenu() {
-            console.log("Toggled menu to " + this.menuExpanded);
-            this.menuExpanded = !this.menuExpanded;
+                console.log("Toggled menu to " + this.menuExpanded);
+                this.menuExpanded = !this.menuExpanded;
             },
+            updateIsMobile() { 
+                this.isMobile = window.innerWidth < 1024;
+            }
+        },
+        mounted() { 
+            window.addEventListener('resize', this.updateIsMobile);
+        },
+        destroyed() {
+            window.removeEventListener('resize', this.updateIsMobile);
         }
     }
 </script>
@@ -17,13 +27,13 @@
 <template>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div class="navbar">
-        <a href="javascript:void(0);" class="toggle" @click="toggleMenu()">
+        <a v-if="isMobile" href="javascript:void(0);" :class="{toggle: isMobile}" @click="toggleMenu()">
             <i class="fa fa-bars"></i>
         </a>
-        <a v-if="menuExpanded" href="" class="nav-item">Home</a>
-        <a v-if="menuExpanded" href="" class="nav-item">About Us</a>
-        <a v-if="menuExpanded" href="" class="nav-item">Contact Us</a>
-        <a v-if="menuExpanded" href="" class="nav-item">FAQ</a>
+        <a v-if="!isMobile || menuExpanded" href="" class="nav-item">Home</a>
+        <a v-if="!isMobile || menuExpanded" href="" class="nav-item">About Us</a>
+        <a v-if="!isMobile || menuExpanded" href="" class="nav-item">Contact Us</a>
+        <a v-if="!isMobile || menuExpanded" href="" class="nav-item">FAQ</a>
     </div>
 </template>
 
