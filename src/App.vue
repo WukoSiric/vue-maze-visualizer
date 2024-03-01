3bc7248d9e0c799229d1e1c67ce0151b93fb0e0d
WukoSiric <lstainer2002@gmail.com>
2024-03-02 02:24:21 +1030

    Buttons trigger functions and refactor app to options API

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
-<script setup>
+<script>
 import NavBar from './components/NavBar.vue'
 import MazeGrid from './components/MazeGrid.vue';
-import { ref } from 'vue';
+import FloatingButton from './components/FloatingButton.vue';
 
-const generateMaze = () => {
-  console.log('Generating maze');
-  mazeGridRef.value.generateMaze();
-}
-
-const solveMaze = (eventInfo) => {
-  const solvingAlgorithm = eventInfo;
-  mazeGridRef.value.solveMaze(solvingAlgorithm);
-}
-
-const mazeGridRef = ref(null);
+export default {
+  data() {
+    return {
+      mazeGridRef: null,
+    };
+  },
+  methods: {
+    generateMaze() {
+      console.log('Generating maze');
+      this.mazeGridRef.generateMaze();
+    },
+    solveMaze(eventInfo = "DFS") {
+      const solvingAlgorithm = eventInfo;
+      this.mazeGridRef.solveMaze(solvingAlgorithm);
+    },
+  },
+  mounted() {
+    this.mazeGridRef = this.$refs.mazeGridRef;
+  },
+  components: {
+    NavBar,
+    MazeGrid,
+    FloatingButton,
+  },
+};
 </script>
 
 <template>
@@ -22,16 +36,16 @@ const mazeGridRef = ref(null);
   </header>
   <main>
     <MazeGrid ref="mazeGridRef"></MazeGrid>
+    <FloatingButton buttonText="Generate" :buttonFunction="generateMaze"></FloatingButton>
+    <FloatingButton buttonText="Solve" :buttonFunction="solveMaze"></FloatingButton>
   </main>
 </template>
 
 <style scoped>
-
-  @media (min-width: 1024px) {
-    main {
-      padding-left: 80px;
-      margin: 4% 120px;
-    }
+@media (min-width: 1024px) {
+  main {
+    padding-left: 80px;
+    margin: 4% 120px;
   }
-
+}
 </style>
---
+++ src/components/FloatingButton.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+	props: {
+		buttonText: {
+			Type: String,
+			Required: true
+		},
+		buttonFunction: {
+			Type: Function,
+			Required: true
+		}
+	},
+	data() {
+		return {
+			buttonRef: null
+		};
+	},
+	mounted() {
+		this.buttonRef = this.$refs.button;
+	},
+	methods: {
+
+	}
+}
+</script>
+
+<template>
+	<button ref="button" @click="buttonFunction"> {{ buttonText }} </button>
+</template>
+
+<style></style>
