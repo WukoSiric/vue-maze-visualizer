05cf7356ce5e5abbec0648fd2c7fa49fb0c8b522
WukoSiric <lstainer2002@gmail.com>
2024-03-01 20:31:04 +1030

    updated base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-maze-visualizer",
+  base: "/",
   plugins: [
     vue(),
     vueJsx(),
